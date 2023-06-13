074a63e7273f80f5728117452624458d4c0758e9
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2023-06-13 20:36:53 +0000

    maybe this will do?

--- main.go
+++ main.go
@@ -6,32 +6,44 @@ import (
 	"time"
 )
 
-// Philosopher is a struct wich stores information about a philosopher.
+// The Dining Philosophers problem is well known in computer science circles.
+// Five philosophers, numbered from 0 through 4, live in a house where the
+// table is laid for them; each philosopher has their own place at the table.
+// Their only difficulty – besides those of philosophy – is that the dish
+// served is a very difficult kind of spaghetti which has to be eaten with
+// two forks. There are two forks next to each plate, so that presents no
+// difficulty. As a consequence, however, this means that no two neighbours
+// may be eating simultaneously, since there are five philosophers and five forks.
+//
+// This is a simple implementation of Dijkstra's solution to the "Dining
+// Philosophers" dilemma.
+
+// Philosopher is a struct which stores information about a philosopher.
 type Philosopher struct {
 	name      string
 	rightFork int
 	leftFork  int
 }
 
-// Philosophers is a list of all philosophers.
+// philosophers is list of all philosophers.
 var philosophers = []Philosopher{
 	{name: "Plato", leftFork: 4, rightFork: 0},
-	{name: "Socratis", leftFork: 0, rightFork: 1},
+	{name: "Socrates", leftFork: 0, rightFork: 1},
 	{name: "Aristotle", leftFork: 1, rightFork: 2},
 	{name: "Pascal", leftFork: 2, rightFork: 3},
-	{name: "Lock", leftFork: 3, rightFork: 4},
+	{name: "Locke", leftFork: 3, rightFork: 4},
 }
 
-// define some variables
-var hunger = 3 // how many times does a person eat?
-var eatTime = 1 * time.Second
-var thinkTime = 3 * time.Second
-var sleepTime = 1 * time.Second
+// Define a few variables.
+var hunger = 3                  // how many times a philosopher eats
+var eatTime = 1 * time.Second   // how long it takes to eatTime
+var thinkTime = 3 * time.Second // how long a philosopher thinks
+var sleepTime = 1 * time.Second // how long to wait when printing things out
 
 func main() {
 	// print out a welcome message
-	fmt.Println("Dinning Philosophers Problem")
-	fmt.Println("----------------------------")
+	fmt.Println("Dining Philosophers Problem")
+	fmt.Println("---------------------------")
 	fmt.Println("The table is empty.")
 
 	// start the meal
@@ -43,27 +55,64 @@ func main() {
 }
 
 func dine() {
+	// wg is the WaitGroup that keeps track of how many philosophers are still at the table. When
+	// it reaches zero, everyone is finished eating and has left. We add 5 (the number of philosophers) to this
+	// wait group.
 	wg := &sync.WaitGroup{}
 	wg.Add(len(philosophers))
 
+	// We want everyone to be seated before they start eating, so create a WaitGroup for that, and set it to 5.
 	seated := &sync.WaitGroup{}
 	seated.Add(len(philosophers))
 
-	// forks is a map of all 5 forks.
+	// forks is a map of all 5 forks. Forks are assigned using the fields leftFork and rightFork in the Philosopher
+	// type. Each fork, then, can be found using the index (an integer), and each fork has a unique mutex.
 	var forks = make(map[int]*sync.Mutex)
 	for i := 0; i < len(philosophers); i++ {
 		forks[i] = &sync.Mutex{}
 	}
 
-	// start the meal.
+	// Start the meal by iterating through our slice of Philosophers.
 	for i := 0; i < len(philosophers); i++ {
-		//fire off a go routine for the current philosopher
+		// fire off a goroutine for the current philosopher
 		go diningProblem(philosophers[i], wg, forks, seated)
 	}
 
+	// Wait for the philosophers to finish. This blocks until the wait group is 0.
 	wg.Wait()
 }
 
+// diningProblem is the function fired off as a goroutine for each of our philosophers. It takes one
+// philosopher, our WaitGroup to determine when everyone is done, a map containing the mutexes for every
+// fork on the table, and a WaitGroup used to pause execution of every instance of this goroutine
+// until everyone is seated at the table.
 func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
 	defer wg.Done()
+
+	// seat the philosopher at the table
+	fmt.Printf("%s is seated at the table.\n", philosopher.name)
+	seated.Done()
+	seated.Wait()
+
+	// eat "hunger" times
+	for i := hunger; i > 0; i-- {
+		// get a lock on both forks
+		forks[philosopher.leftFork].Lock()
+		fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+		forks[philosopher.rightFork].Lock()
+		fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+
+		fmt.Printf("\t%s has both forks and is eating.\n", philosopher.name)
+		time.Sleep(eatTime)
+
+		fmt.Printf("\t%s is thinking.\n", philosopher.name)
+		time.Sleep(thinkTime)
+
+		forks[philosopher.leftFork].Unlock()
+		forks[philosopher.rightFork].Unlock()
+
+		fmt.Printf("\t%s put down the forks.\n", philosopher.name)
+	}
+	fmt.Println(philosopher.name, "is satisfied.")
+	fmt.Println(philosopher.name, "left the table.")
 }
