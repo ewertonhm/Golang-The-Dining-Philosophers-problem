90b7cb28601a72dac7e8ff5bc644874813ae67ad
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2023-06-13 19:58:55 +0000

    initial scope and some inital logic

--- main.go
+++ main.go
@@ -1,5 +1,69 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Philosopher is a struct wich stores information about a philosopher.
+type Philosopher struct {
+	name      string
+	rightFork int
+	leftFork  int
+}
+
+// Philosophers is a list of all philosophers.
+var philosophers = []Philosopher{
+	{name: "Plato", leftFork: 4, rightFork: 0},
+	{name: "Socratis", leftFork: 0, rightFork: 1},
+	{name: "Aristotle", leftFork: 1, rightFork: 2},
+	{name: "Pascal", leftFork: 2, rightFork: 3},
+	{name: "Lock", leftFork: 3, rightFork: 4},
+}
+
+// define some variables
+var hunger = 3 // how many times does a person eat?
+var eatTime = 1 * time.Second
+var thinkTime = 3 * time.Second
+var sleepTime = 1 * time.Second
+
+func main() {
+	// print out a welcome message
+	fmt.Println("Dinning Philosophers Problem")
+	fmt.Println("----------------------------")
+	fmt.Println("The table is empty.")
+
+	// start the meal
+	dine()
+
+	// print out finished message
+	fmt.Println("The table is empty.")
+
+}
+
+func dine() {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosophers))
+
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosophers))
+
+	// forks is a map of all 5 forks.
+	var forks = make(map[int]*sync.Mutex)
+	for i := 0; i < len(philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	// start the meal.
+	for i := 0; i < len(philosophers); i++ {
+		//fire off a go routine for the current philosopher
+		go diningProblem(philosophers[i], wg, forks, seated)
+	}
+
+	wg.Wait()
+}
+
+func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
+	defer wg.Done()
 }
